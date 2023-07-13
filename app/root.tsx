b6ef8ae92890291769466f631c83a8142a2c3201
Matt Silverlock <matt@eatsleeprepeat.net>
2023-07-13 12:01:42 -0400

    better favicon

--- app/root.tsx
+++ app/root.tsx
@@ -11,6 +11,11 @@ import {
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
+  {
+    rel: "icon",
+    href: "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%2016%2016'%3E%3Ctext%20x='0'%20y='14'%3E🦄%3C/text%3E%3C/svg%3E",
+    type: "image/svg+xml",
+  },
 ];
 
 export const meta: V2_MetaFunction = ({ matches, location, params, data }) => {
